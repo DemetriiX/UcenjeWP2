e74accd1d89d362cdb926e0e67cac0df752b65ae
Domagoj Veseli <dveseli555@gmail.com>
2024-01-09 13:23:01 +0100

    zimski zadaci

---
+++ ZimskoVjezbanje/V01ZimskoVjezbanje.cs
@@ -0,0 +1,72 @@
+﻿namespace UcenjeCS
+{
+    internal class V01ZimskoVjezbanje
+    {
+        public static void Izvedi()
+        {
+            // 1. Napisati program koji ispisuje sve brojeve od 1 do 100
+
+            //for (int i = 1; i <= 100; i++) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            // 2. Napisati program koji ispisuje sve brojeve od 100 do 1 
+
+            //for (int i = 100; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            // 3. Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7
+
+            //for (int i  = 1; i <= 100;  i++)
+            //{
+            //    if (i%7 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            // 4. Napisati program koji unosi brojeve sve dok ne unese broj veći od 100, a zatim ispisuje koliko je bilo pokušaja unosa
+
+            int targetNumber = 100;
+            int attempts = 0;
+            int userInput;
+
+            do
+            {
+                Console.Write("Unesite broj veći od 100: ");
+
+                if (int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    attempts++;
+
+                    if (userInput <= targetNumber)
+                    {
+                        Console.WriteLine("Niste unijeli broj veći od 100, pokušajte ponovno. ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Vaš broj je: {userInput}.");
+                    }
+                }
+                else
+                {
+                    attempts++;
+                    Console.WriteLine("Niste unijeli cjelobrojni broj, pokušajte ponovno. ");
+                }
+
+            } while (userInput <= targetNumber);
+
+            Console.WriteLine($"Trebalo vam je {attempts} puta da unesete broj veći od {targetNumber}.");
+
+
+
+
+        }
+    }
+}
---
+++ ZimskoVjezbanje/V02ZimskoVjezbanje.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("V02")]
+    public class V02ZimskoVjezbanje:ControllerBase
+    {
+        [HttpGet]
+        [Route("zad1")]
+        public IActionResult GetSum()
+        {
+            // Ruta ne prima niti jedan parametar i vraća zbroj prvih 100 brojeva
+            int sum = 0;
+            for (int i = 1;  i <= 100; i++) 
+            {
+                sum += i;
+            }
+
+            return Ok(sum);
+        }
+
+        [HttpGet]
+        [Route("zad2")]
+
+        public IActionResult GetEvenNumbers()
+        {
+            // Ruta ne prima niti jedan parametar i vraća niz s svim parnim brojevima od 1 do 57            
+            int[] evenNumbers = Enumerable.Range(1, 57).Where(x => x % 2 == 0).ToArray();
+
+            return Ok(evenNumbers);
+
+        }
+
+        [HttpGet]
+        [Route("zad3")]
+
+        public IActionResult GetEvenSum()
+        {
+            // Ruta ne prima niti jedan parametar i vraća zbroj svih parnih brojeva od 2 do 18
+
+            int sum = 0;
+            for (int i = 2; i <= 18 && i % 2 == 0; i += 2)
+            {
+                sum += i;
+            }
+
+            return Ok(sum);
+        }
+
+        [HttpGet]
+        [Route("zad4")]
+
+        public IActionResult GetUserSum(int broj)
+        {
+            // Ruta prima jedan parametar koji je cijeli broj i vraća zbroj svih brojeva od 1 do primljenog broja            
+
+            int sum = 0;
+            for (int i = 0; i <= broj; i++)
+            {
+                sum += i;
+            }
+
+            return Ok(sum);
+        }
+
+        [HttpGet]
+        [Route("zad5")]
+
+        public IActionResult GetTwoUser(int broj1, int broj2)
+        {
+            // Ruta prima dva parametra koji su cijeli brojevi i vraća niz s svim parnim brojevima između primljenih brojeva
+            // PITAJ PROFESORA - npr. sa "25" kao drugi broj, vrati "26" kao zadnji broj
+
+            int[] evenNumbers = Enumerable.Range(broj1, broj2).Where(x => x % 2 == 0).ToArray();
+
+            return Ok(evenNumbers);
+
+        }
+
+        [HttpGet]
+        [Route("zad6")]
+
+        public IActionResult GetAnotherTwoUser(int broj1, int broj2)
+        {
+            // Ruta prima dva parametra koji su cijeli brojevi i vraća niz s svim neparnim brojevima između primljenih brojeva
+
+            int[] oddNumbers = Enumerable.Range(broj1, broj2).Where(x => x % 2 != 0).ToArray();
+
+            return Ok(oddNumbers);
+        }
+
+        [HttpGet]
+        [Route("zad7")]
+
+        public IActionResult GetMoreTwoUser(int broj1, int broj2)
+        {
+            // Ruta prima dva parametra koji su cijeli brojevi i vraća zbroj svih brojeva između primljenih brojeva
+
+            int sum = 0;
+            for (int i = broj1; i <= broj2; i++)
+            {
+                sum += i;
+            }
+
+            return Ok(sum);
+        }
+
+        [HttpGet]
+        [Route("zad8")]
+
+        public IActionResult GetTwoUserAgain(int broj1, int broj2)
+        {
+            // Ruta prima dva parametra koji su cijeli brojevi i vraća zbroj svih brojeva između primljenih brojeva koji su cjelobrojno djeljivi s 3
+
+            int sum = 0;
+            for (int i = broj1; i <= broj2; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }               
+            }
+
+            return Ok(sum);
+        }
+
+        [HttpGet]
+        [Route("zad9")]
+
+        public IActionResult GetSumDivisible(int broj1, int broj2)
+        {
+            // Ruta prima dva parametra koji su cijeli brojevi i vraća zbroj svih brojeva između primljenih brojeva koji su cjelobrojno djeljivi s 3 i 5
+
+            int sum = 0;
+            for (int i = broj1; i <= broj2; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return Ok(sum);
+        }
+
+        [HttpGet]
+        [Route("zad10")]
+
+        public IActionResult GetTable(int DoKojegBroja, int DoKolikoPutaSeMnozi)
+        {
+            // Ruta prima dva parametra koji su cijeli brojevi i vraća dvodimenzionalni niz (matricu) koja sadrži tablicu množenja za dva primljena broja
+
+            //int[,] multiplicationTable = new int[DoKojegBroja,DoKolikoPutaSeMnozi];
+
+            //for (int i = 1; i <= DoKojegBroja; i++)
+            //{
+            //    for (int j = 1; j <= DoKolikoPutaSeMnozi; j++)
+            //    {
+            //        multiplicationTable[i - 1, j - 1] = i * j;
+            //    }
+            //}
+
+            //return Ok(new { MultiplicationTable = multiplicationTable });
+            int[][] multiplicationTable = new int[DoKojegBroja][];
+
+            for (int i = 0; i < DoKojegBroja; i++)
+            {
+                multiplicationTable[i] = new int[DoKolikoPutaSeMnozi];
+                for (int j = 0; j < DoKolikoPutaSeMnozi; j++)
+                {
+                    multiplicationTable[i][j] = (i + 1) * (j + 1);
+                }
+            }
+
+            return Ok(multiplicationTable);
+        }
+    }
+}
